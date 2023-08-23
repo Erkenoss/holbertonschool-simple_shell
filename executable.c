93a1fd0a258d35212880de94fce1eb2c1d738ae9
Erkenoss <xavier.bertin6@gmail.com>
2023-08-23 15:45:40 +0000

    add error msg

--- executable.c
+++ executable.c
@@ -11,8 +11,8 @@ void executable(char *argv[])
 
 	if (access(argv[0], X_OK) != 0)
 	{
-		perror("file not found");
-		return;
+		printf("./hsh: 1: %s: not found\n", argv[0]);
+		exit(127);
 	}
 	child = fork();
 
