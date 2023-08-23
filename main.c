0e6f3a587a6836877eb844814cc9ad34acc2d931
SpStigma <GitHubStigma@gmail.com>
2023-08-23 05:16:42 -0700

    fix errors ctr d

--- main.c
+++ main.c
@@ -12,26 +12,37 @@ int main(void)
 	char *input = NULL;
 	size_t len_input = 0;
 	ssize_t read = 0;
-	int is_interactive = isatty(STDIN_FILENO);	
+	int is_interactive = isatty(STDIN_FILENO);
+	int is_empty = 1;
+	size_t index;
 
 	while (1)
 	{
 		if (is_interactive)
 		{
 			printf("$ ");
+			fflush(stdout);
 		}
 
 		read = getline(&input, &len_input, stdin);
 		if (read == -1)
 		{
 			free(input);
-			printf("\n");
 			exit(EXIT_FAILURE);
 		}
-		if (read > 0 && input[read - 1] == '\n')
-		{
-   			input[read - 1] = '\0';
+		if (read > 0 && input[read - 1] == '\n') {
+			input[read - 1] = '\0';
 		}
+		is_empty = 1;
+		for (index = 0; index < len_input; index++) 
+		{
+			if (input[index] != '\0' && input[index] != '\n') 
+			{
+				is_empty = 0;
+				break;
+			}
+		}
+		if (!is_empty)
 		token_input(input);
 	}
 	free(input);
