60ea4de5af2e40aa0f70f43be63b8149a8c31a1b
Erkenoss <xavier.bertin6@gmail.com>
2023-08-24 11:00:57 -0700

    Add 1/2 exit

--- tokens.c
+++ tokens.c
@@ -25,6 +25,11 @@ void token_input(char *input)
 		print_env();
 		return;
 	}
+	if (strcmp(tokens[0], "exit") == 0)
+	{
+		free(tokens[0]);
+		exit(0);
+	}
 	if (strchr(tokens[0], '/') == NULL)
 	{
 		tokens[0] = _getpath(tokens[0]);
