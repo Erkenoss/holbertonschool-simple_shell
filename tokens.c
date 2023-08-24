35fc31e757f1c1a67540512b7c1bece9e8f73fd1
SpStigma <GitHubStigma@gmail.com>
2023-08-24 06:03:41 -0700

    Fix: Handle error PATH1

--- tokens.c
+++ tokens.c
@@ -5,8 +5,9 @@ void token_input(char *input)
 	char *split = NULL;
 	char *tokens[1024] = { NULL };
 	int index = 0;
+	char *final_path = NULL;
 
-	split = strtok(input, " ");
+		split = strtok(input, " ");
 	while (split)
 	{
 		if (strlen(split) > 0)
@@ -25,14 +26,18 @@ void token_input(char *input)
 		print_env();
 		return;
 	}
-	     if (strchr(tokens[0], '/') == NULL)
-        {
-                tokens[0] = _getpath(tokens[0]);
-                executable(tokens);
-                free(tokens[0]);
-        }
-        else
-        {
-                executable(tokens);
-        }
+	if (strchr(tokens[0], '/') == NULL)
+	{
+		final_path = _getpath(tokens[0]);
+		if(final_path != NULL)
+		{
+			tokens[0] = strdup(final_path);
+			executable(tokens);
+			free(tokens[0]);
+		}
+	}
+	else
+	{
+		executable(tokens);
+	}
 }
