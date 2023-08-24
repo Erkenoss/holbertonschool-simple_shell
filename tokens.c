3634df197356e1193cc7451766dfe96f41a5424c
SpStigma <GitHubStigma@gmail.com>
2023-08-24 06:12:34 -0700

    fix: Error PATH1

--- tokens.c
+++ tokens.c
@@ -5,9 +5,8 @@ void token_input(char *input)
 	char *split = NULL;
 	char *tokens[1024] = { NULL };
 	int index = 0;
-	char *final_path = NULL;
 
-		split = strtok(input, " ");
+	split = strtok(input, " ");
 	while (split)
 	{
 		if (strlen(split) > 0)
@@ -28,13 +27,9 @@ void token_input(char *input)
 	}
 	if (strchr(tokens[0], '/') == NULL)
 	{
-		final_path = _getpath(tokens[0]);
-		if(final_path != NULL)
-		{
-			tokens[0] = strdup(final_path);
-			executable(tokens);
-			free(tokens[0]);
-		}
+		tokens[0] = _getpath(tokens[0]);
+		executable(tokens);
+		free(tokens[0]);
 	}
 	else
 	{
